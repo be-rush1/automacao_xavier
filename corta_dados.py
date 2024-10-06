8628da296fde611855d86985f73bc0297e4d6e02
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-10-06 17:07:34 -0300

    Update corta_dados.py

--- corta_dados.py
+++ corta_dados.py
@@ -9,6 +9,6 @@ path = "dados_extraidos/" + sys.argv[1]
 data = xr.open_dataset(path)
 data.rio.set_spatial_dims(x_dim="latitude", y_dim="longitude", inplace=True)
 data.rio.write_crs("epsg:4326", inplace=True)
-sudeste = geopandas.read_file('BR_região_sudeste_2022.shp', crs="epsg:4326")
+sudeste = geopandas.read_file('./BR_região_sudeste_2022.shp', crs="epsg:4326")
 clipped = data.rio.clip(sudeste.geometry.apply(mapping), sudeste.crs, drop=True)
 clipped.to_netcdf("dados_cortados_" + arquivo + ".nc")
