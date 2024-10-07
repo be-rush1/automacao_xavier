850ba281d482b87d2b1c66dac84075641903c7cb
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-10-07 15:09:42 -0300

    Update converte.py

--- converte.py
+++ converte.py
@@ -1,10 +1,19 @@
-import numpy
-import xarray
-import pandas
+import numpy as np
+import xarray as xr
+import pandas as pd
 
+# Carregar o arquivo .npy
 fname = "data.npy"
-arr = numpy.load(fname)
-df = pandas.DataFrame({"lon":arr[:,0], "lat":arr[:,1], "data":arr[:,3]})
-df = df[~df.index.duplicated(keep='first')]
-ds = df.set_index(["lat","lon"]).to_xarray()
+arr = np.load(fname)
+
+# Criar um DataFrame com as colunas "lon", "lat" e "data"
+df = pd.DataFrame({"lon": arr[:, 0], "lat": arr[:, 1], "data": arr[:, 3]})
+
+# Remover duplicatas baseadas nas colunas "lat" e "lon"
+df = df.drop_duplicates(subset=["lat", "lon"])
+
+# Converter o DataFrame para um xarray.Dataset
+ds = df.set_index(["lat", "lon"]).to_xarray()
+
+# Salvar como arquivo .nc (NetCDF)
 ds.to_netcdf("data.nc")
