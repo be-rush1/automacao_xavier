0466815c9592b4f39f5a0b6556c2a430a5f65697
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-10-07 15:05:34 -0300

    Update converte.py

--- converte.py
+++ converte.py
@@ -5,5 +5,6 @@ import pandas
 fname = "data.npy"
 arr = numpy.load(fname)
 df = pandas.DataFrame({"lon":arr[:,0], "lat":arr[:,1], "data":arr[:,3]})
+df = df[~df.index.duplicated(keep='first')]
 ds = df.set_index(["lat","lon"]).to_xarray()
 ds.to_netcdf("data.nc")
