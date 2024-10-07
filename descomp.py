476df5384398d7e3b8ae03317378d42f12460ce1
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-10-07 14:05:08 -0300

    Create descomp.py

---
+++ descomp.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Carrega o arquivo .npz
+data = np.load('seu_arquivo.npz')
+
+# Exibe as chaves (nomes dos arrays contidos no arquivo)
+print(data.files)
+
+# Acessa cada array pelo nome da chave
+for key in data.files:
+    print(f"Array '{key}':\n{data[key]}\n")
+
+for key in data.files:
+    np.save(f'{key}.npy', data[key])  # Salva cada array em formato .npy
