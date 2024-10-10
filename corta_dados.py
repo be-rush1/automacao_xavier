eff07f5a09cd70603f4311e787e40b63ee426de4
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-10-09 22:27:15 -0300

    Update corta_dados.py

--- corta_dados.py
+++ corta_dados.py
@@ -6,9 +6,9 @@ import os
 from shapely.geometry import mapping
 path = sys.argv[1]
 data = xr.open_dataset(path)
-print(data.dims)
 data.rio.set_spatial_dims(x_dim="latitude", y_dim="longitude", inplace=True)
 data.rio.write_crs("epsg:4326", inplace=True)
+print(data.variables)
 sudeste = geopandas.read_file('BR_região_sudeste_2022.shp', crs="epsg:4326")
 clipped = data.rio.clip(sudeste.geometry.apply(mapping), sudeste.crs, drop=True)
 clipped.to_netcdf("dados_cortados_" + sys.argv[1] + ".nc")
