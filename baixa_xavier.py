138eac2f769d7065e419998fa4a208c90d2f1859
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-12-16 11:39:37 -0300

    Update baixa_xavier.py

--- baixa_xavier.py
+++ baixa_xavier.py
@@ -1,7 +1,7 @@
 import gdown
 
 # URL do arquivo do Google Drive
-url = 'https://drive.google.com/uc?id=1ATAtgnG2_zSfar1TAKu3SIn5c0RsBHJG'  
+url = 'https://drive.google.com/uc?id=11-qnvwojirAtaQxSE03N0_SUrbcsz44N'  
 output = 'pr.npz' 
 
 # Baixar o arquivo
