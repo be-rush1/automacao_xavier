e127236c9f3d6e83067082d61b63bb405aee0431
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-12-16 11:54:09 -0300

    Update baixa_xavier.py

--- baixa_xavier.py
+++ baixa_xavier.py
@@ -2,7 +2,7 @@ import gdown
 
 # URL do arquivo do Google Drive
 url = 'https://drive.google.com/uc?id=11yTPRMF9RgyF4irAWWydm8IKzlr3eI8_'  
-output = 'pr.npz' 
+output = 'pr_Tmax_Tmin_NetCDF_Files.zip' 
 
 # Baixar o arquivo
 gdown.download(url, output, quiet=False)
