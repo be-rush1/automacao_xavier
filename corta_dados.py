ce8c1e408be2f3781dd7d71b3483b3e04fde0043
bernardo <bernardo.nunes42@gmail.com>
2025-06-24 09:08:48 -0300

    corrije corte de dados

--- corta_dados.py
+++ corta_dados.py
@@ -1,34 +1,15 @@
 import rioxarray
 import xarray as xr
-import geopandas as gpd
+import geopandas
 import sys
 import os
 from shapely.geometry import mapping
-
-# Caminho do arquivo NetCDF passado via linha de comando
 path = sys.argv[1]
-
-# Abrir o dataset
 data = xr.open_dataset(path)
-
-# Corrigir a ordem das dimensões espaciais: longitude = x, latitude = y
 data.rio.set_spatial_dims(x_dim="longitude", y_dim="latitude", inplace=True)
-
-# Definir o sistema de referência de coordenadas (CRS)
-data.rio.write_crs("EPSG:4326", inplace=True)
-
-# Remover variável de limites de tempo, se existir
+data.rio.write_crs("epsg:4326", inplace=True)
 data = data.drop_vars("time_bnds", errors="ignore")
-
-# Ler o shapefile da região sudeste (corrigir possível problema de codificação no nome)
-shapefile_path = 'BR_região_sudeste_2022.shp'
-sudeste = gpd.read_file(shapefile_path)
-
-# Clipping com geometria do shapefile
+sudeste = geopandas.read_file('BR_região_sudeste_2022.shp', crs="epsg:4326")
 clipped = data.rio.clip(sudeste.geometry.apply(mapping), sudeste.crs, drop=True)
-
-# Nome do arquivo de saída
-output_name = "dados_cortados_" + os.path.basename(path)
-clipped.to_netcdf(output_name)
-
-print("Dados de " + path + " cortados com sucesso! Arquivo salvo como " + output_name)
+clipped.to_netcdf("dados_cortados_" + sys.argv[1])
+print("Dados de " + sys.argv[1] + " cortados com sucesso!")
