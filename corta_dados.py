dea30af90ac27076d5c6c653bfd1935490086f9f
bernardo <bernardo.nunes42@gmail.com>
2025-06-23 21:00:55 -0300

    corrige

--- corta_dados.py
+++ corta_dados.py
@@ -4,36 +4,12 @@ import geopandas
 import sys
 import os
 from shapely.geometry import mapping
-
-# Argumentos: 1 = arquivo NetCDF, 2 = shapefile
 path = sys.argv[1]
-shapefile = sys.argv[2]
-
-# Carrega os dados climáticos
 data = xr.open_dataset(path)
-
-# Define as dimensões espaciais e sistema de referência
 data.rio.set_spatial_dims(x_dim="latitude", y_dim="longitude", inplace=True)
 data.rio.write_crs("epsg:4326", inplace=True)
-
-# Remove a variável 'time_bnds' se existir
 data = data.drop_vars("time_bnds", errors="ignore")
-
-# Lê o shapefile passado como argumento
-sudeste = geopandas.read_file(shapefile)
-
-# Garante que o shapefile tenha um CRS definido
-if sudeste.crs is None:
-    sudeste.set_crs("epsg:4326", inplace=True)
-
-# Converte o CRS do shapefile para o mesmo do dataset
-sudeste = sudeste.to_crs(data.rio.crs)
-
-# Recorta os dados
+sudeste = geopandas.read_file('BR_região_sudeste_2022.shp', crs="epsg:4326")
 clipped = data.rio.clip(sudeste.geometry.apply(mapping), sudeste.crs, drop=True)
-
-# Salva o arquivo recortado
-saida = "dados_cortados_" + os.path.basename(path)
-clipped.to_netcdf(saida)
-
-print("Dados de " + path + " cortados com sucesso!")
+clipped.to_netcdf("dados_cortados_" + sys.argv[1])
+print("Dados de " + sys.argv[1] + " cortados com sucesso!")
