c3c109f655432c7bc7e656d1c4564f7ccc9f86ed
bernardo <bernardo.nunes42@gmail.com>
2025-06-24 09:29:22 -0300

    corrige caminho

--- novo.py
+++ novo.py
@@ -4,9 +4,9 @@ import xarray as xr
 
 # Abrir os arquivos NetCDF
 arquivos = [
-    '/Users/elizabetenunes/Desktop/dados_cortados_media_pr_20010101_20240320_BR-DWGD_UFES_UTEXAS_v_3.2.3.nc',
-    '/Users/elizabetenunes/Desktop/dados_cortados_media_pr_19810101_20001231_BR-DWGD_UFES_UTEXAS_v_3.2.3.nc',
-    '/Users/elizabetenunes/Desktop/dados_cortados_media_pr_19610101_19801231_BR-DWGD_UFES_UTEXAS_v_3.2.3.nc'
+    'dados_cortados_media_pr_20010101_20240320_BR-DWGD_UFES_UTEXAS_v_3.2.3.nc',
+    'dados_cortados_media_pr_19810101_20001231_BR-DWGD_UFES_UTEXAS_v_3.2.3.nc',
+    'dados_cortados_media_pr_19610101_19801231_BR-DWGD_UFES_UTEXAS_v_3.2.3.nc'
 ]
 
 dataset_combinado = xr.open_mfdataset(arquivos, combine='by_coords')
